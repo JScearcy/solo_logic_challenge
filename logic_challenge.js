9e513ba36e3cbca14bcea70b96304e4f008fd792
JScearcy <jacobscearcy@gmail.com>
2015-08-18 20:31:30 -0500

    Second commit, second challenge done

--- logic_challenge.js
+++ logic_challenge.js
@@ -9,3 +9,26 @@ var primes = [2, 3, 5, 7, 11, 13, 17, 19];
 var nonPrimes = [1, 4, 6, 8, 9, 10, 12, 14];
 
 console.log(addAndReverseArray(primes, nonPrimes));
+
+function combineAndSort(array, secondArray){
+  var combinedSort = [];
+  for(var i = 0; i < array.length; i++) {
+    combinedSort.push(array[i]);
+    combinedSort.push(secondArray[i]);
+  }
+  combinedSort.sort(function(a, b){
+    if(a > b){
+      return 1
+    }
+    else if(a < b){
+      return -1
+    }
+    else {
+      return 0
+    }
+  })
+  return combinedSort;
+}
+var numbers1 = [4, 1, 6, 5, 8];
+var numbers2 = [7, 3, 2, 9, 10];
+console.log(combineAndSort(numbers1, numbers2))
