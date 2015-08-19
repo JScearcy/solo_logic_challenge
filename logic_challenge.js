a03988f5f75bbba388ec98188f783c802e4fd036
JScearcy <jacobscearcy@gmail.com>
2015-08-18 20:22:31 -0500

    First commit, first challenge done

--- logic_challenge.js
+++ logic_challenge.js
@@ -1,5 +1,3 @@
-var primes = [2, 3, 5, 7, 11, 13, 17, 19];
-var nonPrimes = [1, 4, 6, 8, 9, 10, 12, 14];
 function addAndReverseArray(array, arrayReverse){
   var combined = [];
   for(var i = 0; i < array.length; i++) {
@@ -7,4 +5,7 @@ function addAndReverseArray(array, arrayReverse){
   }
   return combined;
 }
+var primes = [2, 3, 5, 7, 11, 13, 17, 19];
+var nonPrimes = [1, 4, 6, 8, 9, 10, 12, 14];
+
 console.log(addAndReverseArray(primes, nonPrimes));
