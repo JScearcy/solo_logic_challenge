9f7f93f1822db6c550dcf0f8bc4b5e40c5c338b8
JScearcy <jacobscearcy@gmail.com>
2015-08-18 20:41:59 -0500

    Third commit, third challenge done

--- logic_challenge.js
+++ logic_challenge.js
@@ -7,7 +7,6 @@ function addAndReverseArray(array, arrayReverse){
 }
 var primes = [2, 3, 5, 7, 11, 13, 17, 19];
 var nonPrimes = [1, 4, 6, 8, 9, 10, 12, 14];
-
 console.log(addAndReverseArray(primes, nonPrimes));
 
 function combineAndSort(array, secondArray){
@@ -31,4 +30,18 @@ function combineAndSort(array, secondArray){
 }
 var numbers1 = [4, 1, 6, 5, 8];
 var numbers2 = [7, 3, 2, 9, 10];
-console.log(combineAndSort(numbers1, numbers2))
+console.log(combineAndSort(numbers1, numbers2));
+
+function rightTriangle(a, b, angle) {
+  this.a = a;
+  this.b = b;
+  this.angle = angle;
+}
+rightTriangle.prototype.hypotenuse = function(){
+  if(this.angle == 90){
+    this.c = Math.sqrt(((this.a*this.a) + (this.b*this.b)));
+  }
+}
+var newTriangle = new rightTriangle(3, 4, 90);
+newTriangle.hypotenuse();
+console.log(newTriangle.c);
